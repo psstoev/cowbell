b4c8507745da792b27614215e6935eeef2a21745
Plamen Stoev <psstoev@gmail.com>
2014-04-03 16:19:59 +0300

    Add a dummy game server

---
+++ bin/cowbell-server.js
@@ -0,0 +1,26 @@
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var socketIo = require("socket.io");
+var io;
+
+var app = http.createServer(function(request, response) {
+    fs.readFile(path.resolve(__dirname, "../index.html"), function(err, data) {
+        if (err) {
+            response.writeHead(500);
+            return response.end("Cannot find index.html");
+        }
+
+        response.writeHead(200);
+        response.end(data);
+    });
+});
+
+io = socketIo.listen(app);
+app.listen(8080);
+
+io.sockets.on("connection", function(socket) {
+    socket.on("guess", function(data) {
+        console.log(data);
+    });
+});
