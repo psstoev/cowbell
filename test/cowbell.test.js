6d18577ed060188de9400e30d87cd7941c287d35
Plamen Stoev <psstoev@gmail.com>
2014-04-03 11:37:13 +0300

    Add the getRandomNumber() number function
    It returns random numbers and strings, that obey to the rules of the
    game.

--- test/cowbell.test.js
+++ test/cowbell.test.js
@@ -1,9 +1,40 @@
 var chai = require("chai");
-var expect = chai.expect;
 var cowbell = require("../lib/cowbell");
+var expect = chai.expect;
 
 describe("getRandomNumber()", function() {
-    it("returns 1", function() {
-        expect(cowbell.getRandomNumber()).to.eq(1);
+    it("returns a 4-digit number as a string", function() {
+        expect(cowbell.getRandomNumber()).to.match(/\d{4}/);
+    });
+
+    it("returns a number with unique digits", function() {
+        var sortedResult = cowbell.getRandomNumber().split("").sort().join("");
+
+        expect(sortedResult).to.not.match(/(\d)\1+/);
+    });
+
+    it("returns a number that does not start with 0", function() {
+        expect(cowbell.getRandomNumber()).to.not.match(/^0/);
+    });
+
+    it("returns pseudo-random numbers", function() {
+        /*
+         * NOTE: this test is far from perfect, as it only checks mean values,
+         * but it is better than nothing.
+         *
+         * TODO: move this code in a helper
+         */
+
+        var TOTAL_COUNT = 10000;
+        var EXPECTED_MEAN = 5450;
+        var sum = 0, mean;
+
+        for (var i = 0; i < TOTAL_COUNT; i++) {
+            sum += Number(cowbell.getRandomNumber());
+        }
+
+        mean = sum / TOTAL_COUNT;
+
+        expect(Math.abs(mean - EXPECTED_MEAN)).to.be.below(100);
     });
 });
