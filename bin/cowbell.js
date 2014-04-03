1e4f2435c3e49f486c8bde9d2bf26975b58af1cc
Plamen Stoev <psstoev@gmail.com>
2014-04-03 17:02:54 +0300

    Rewrite the console client using the Game class

--- bin/cowbell.js
+++ bin/cowbell.js
@@ -1,30 +1,29 @@
 #!/usr/bin/env node
 
 var readline = require("readline");
-var cowbell = require("../lib/cowbell");
-var target = cowbell.getRandomNumber();
+var Game = require("../lib/game");
+var game = new Game();
+var gameWon = false;
 
 var readLineInterface = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
-console.log("Hello! Guess my number!");
 readLineInterface.setPrompt("Enter your guess: ");
 readLineInterface.prompt(true);
 
 readLineInterface.on("line", function(guess) {
     var cows, bulls;
 
-    if (cowbell.isGuessValid(guess)) {
-        cows = cowbell.getCowsForGuess(target, guess);
-        bulls = cowbell.getBullsForGuess(target, guess);
+    var result = game.tryGuess(guess);
 
-        if (bulls === 4) {
-            console.log("Correct!");
+    if (!result.error) {
+        if (result.bulls === 4) {
+            gameWon = true;
             readLineInterface.close();
         } else {
-            console.log(cows + " cows and " + bulls + " bulls");
+            console.log(result.cows + " cows and " + result.bulls + " bulls");
         }
     } else {
         console.log("Invalid guess");
@@ -32,6 +31,11 @@ readLineInterface.on("line", function(guess) {
 
     readLineInterface.prompt(true);
 }).on("close", function() {
-    console.log("Bye!");
+    if (!gameWon) {
+        console.log("\nYou surrendered, the number was " + game.target);
+    } else {
+        console.log("\nCorrect! The number was " + game.target + ". Bye!");
+    }
+
     process.exit(0);
 });
