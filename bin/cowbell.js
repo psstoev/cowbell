ee0d3aa26373256653ef88d97b9435347967cdc8
Plamen Stoev <psstoev@gmail.com>
2014-04-03 13:43:47 +0300

    Create a dummy interactive CLI for the game

--- bin/cowbell.js
+++ bin/cowbell.js
@@ -1,1 +1,37 @@
-console.log("Hello, world!");
+#!/usr/bin/env node
+
+var readline = require("readline");
+var cowbell = require("../lib/cowbell");
+var target = cowbell.getRandomNumber();
+
+var readLineInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+console.log("Hello! Guess my number!");
+readLineInterface.setPrompt("Enter your guess: ");
+readLineInterface.prompt(true);
+
+readLineInterface.on("line", function(guess) {
+    var cows, bulls;
+
+    if (cowbell.isGuessValid(guess)) {
+        cows = cowbell.getCowsForGuess(target, guess);
+        bulls = cowbell.getBullsForGuess(target, guess);
+
+        if (bulls === 4) {
+            console.log("Correct!");
+            readLineInterface.close();
+        } else {
+            console.log(cows + " cows and " + bulls + " bulls");
+        }
+    } else {
+        console.log("Invalid guess");
+    }
+
+    readLineInterface.prompt(true);
+}).on("close", function() {
+    console.log("Bye!");
+    process.exit(0);
+});
