3e1818fe33510c39cc9dff490dcb4e52e614410c
Plamen Stoev <psstoev@gmail.com>
2014-04-04 07:59:07 +0300

    Implement simple multiplayer for 3 players
    Limitations:
    
    * if you want to start a new game you have to restart the server.
    * only the player that won will get a notification about this.
    * probably something else, that I'm missing

--- cowbell-server.js
+++ cowbell-server.js
@@ -5,9 +5,21 @@ var fs = require("fs");
 var path = require("path");
 var url = require("url");
 var socketIo = require("socket.io");
-var Game = require("./lib/game");
+var GameSession = require("./lib/game-session");
 var io;
 var playerCount = 0;
+var playerSockets = {};
+var currentPlayer = null;
+
+var gameSession = new GameSession();
+
+gameSession.game.on("playerWon", function() {
+    playerSockets[currentPlayer].emit("correct", { guess: gameSession.game.target });
+}).on("foundCowsAndBulls", function(result) {
+    playerSockets[currentPlayer].emit("found", result);
+}).on("invalidGuess", function(guess) {
+    playerSockets[currentPlayer].emit("error", { guess: guess });
+})
 
 var app = http.createServer(function(request, response) {
     var filename = url.parse(request.url).pathname;
@@ -34,24 +46,34 @@ io = socketIo.listen(app);
 app.listen(8080);
 
 io.sockets.on("connection", function(socket) {
-    var game = new Game();
+    if (gameSession.hasStarted) {
+        socket.emit("already started");
+        return;
+    }
 
     playerCount++;
     var playerId = "player" + playerCount;
 
-    game.on("playerWon", function() {
-        socket.emit("correct", { guess: game.target });
-    }).on("foundCowsAndBulls", function(result) {
-        socket.emit("found", result);
-    }).on("invalidGuess", function(guess) {
-        socket.emit("error", { guess: guess });
-    })
+    socket.emit("welcome", playerId);
+
+    gameSession.addPlayer(playerId);
+    playerSockets[playerId] = socket;
+
+    if (playerCount === 3) {
+        gameSession.start();
+    }
 
     socket.on("guess", function(guess) {
-        game.tryGuess(guess);
+        if (gameSession.hasStarted && gameSession.currentPlayer === playerId) {
+            currentPlayer = playerId;
+            gameSession.game.tryGuess(guess);
+        } else {
+            playerSockets[playerId].emit("wait");
+        }
     }).on("give up", function() {
-        socket.emit("give up", { number: game.target });
+        gameSession.removePlayer(playerId);
+        socket.emit("give up", { number: gameSession.game.target });
+    }).on("disconnect", function() {
+        gameSession.removePlayer(playerId);
     });
-
-    socket.emit("welcome", playerId);
 });
